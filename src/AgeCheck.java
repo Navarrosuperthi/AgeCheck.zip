dccabcf3a827fc8b43fa4e88fbb06fce60a4fc9b
FabioBritto <fabio.tritono@gmail.com>
2024-06-06 13:52:40 -0300

    Commit project AgeCheck.zip

---
+++ src/AgeCheck.java
@@ -0,0 +1,13 @@
+public class AgeCheck {
+   
+    public static void main(String[] args) {
+        int myAge = 15;   // idade para ver se tenho ou não a idade para ter uma carteira de motorista
+        
+        if (myAge >= 16) {
+            System.out.println("Ja tenho idade para ter minha carteira de motorista!");
+        }
+        if (myAge <= 16) {
+            System.out.println("Nao tenho idade o suficiente... :*(");
+        }
+    }
+}
